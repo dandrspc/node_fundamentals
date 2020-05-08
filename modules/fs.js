925cb598f90654ed83594aaf9d7e6d4049485aeb
Daniel Andres Palacios Carabali <dapalca@gmail.com>
2020-05-07 22:34:31 -0500

    Native C++ modules in node

--- modules/fs.js
+++ modules/fs.js
@@ -9,8 +9,8 @@ const read = (path, callback) => {
 const write = (path, data, callback) => {
     fs.writeFile(path, data, (err) => {
         if (err) {
-            console.error('Error while writing ',  err)
-        }else {
+            console.error('Error while writing ', err)
+        } else {
             console.log('File written')
         }
     })
@@ -20,6 +20,8 @@ const deleteFile = (path, callback) => {
     fs.unlink(path, callback)
 
 }
+
+//__direname variable saves the path to the dir
 
 // read(__dirname + '/file.txt', console.log)
 // write(__dirname + '/_file.txt', 'Im a new file', console.log)
